c1a04569673884d2a69747704dee98e04f11f638
Barbara Toïgo <barbara.toigo@isen.yncrea.fr>
2023-12-15 11:46:09 +0100

    Page character

--- src/app/marvel/marvel.component.ts
+++ src/app/marvel/marvel.component.ts
@@ -1,14 +1,14 @@
-// File name: marvel.component.ts
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http'; // Make sure to include this import
+import { HttpClient } from '@angular/common/http';
 
 export interface MarvelData {
   attributionHTML: string;
   data: {
     results: MarvelCharacter[];
+    total: number;
   };
 }
 
@@ -29,21 +29,20 @@ export interface MarvelCharacter {
   templateUrl: './marvel.component.html',
   styleUrls: ['./marvel.component.css']
 })
-
 export class MarvelComponent implements OnInit {
-
-
+  alphabet: string[] = Array.from({ length: 26 }, (_, i) => String.fromCharCode('A'.charCodeAt(0) + i));
+  allCharacters: MarvelCharacter[] = [];
   filteredCharacters: MarvelCharacter[] = [];
   marvelData: MarvelData | undefined;
   private apiUrl = 'http://gateway.marvel.com/v1/public/characters';
   private apiKey = 'eff0bf634828b9b11ad00a5c23f96be3';
-  allCharacters: MarvelCharacter[] = [];
   searchForm: FormGroup;
   searchCtrl: FormControl<string>;
+  currentPage: number = 1;
+  charactersPerPage: number = 72;
+  totalPages: number = 1;
 
- 
-
-  constructor(private http: HttpClient, private formBuilder: FormBuilder) {
+  constructor(private http: HttpClient, private formBuilder: FormBuilder, private zone: NgZone) {
     this.searchCtrl = new FormControl('', { validators: [Validators.required], nonNullable: true });
     this.searchForm = this.formBuilder.group({
       search: this.searchCtrl,
@@ -51,15 +50,13 @@ export class MarvelComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.getMarvelData().subscribe(
-      (data => {
-        this.marvelData = data;
-        this.allCharacters = data.data.results;
-        this.filteredCharacters = this.allCharacters;
-      })
-    );
+    this.getMarvelData().subscribe((data => {
+      this.marvelData = data;
+      this.allCharacters = data.data.results;
+      this.updateTotalPages();
+      this.updateFilteredCharacters();
+    }));
 
-    // Subscribe to changes in the search input
     this.searchCtrl.valueChanges
       .pipe(
         debounceTime(300),
@@ -68,41 +65,89 @@ export class MarvelComponent implements OnInit {
       )
       .subscribe((data: MarvelData) => {
         if (data) {
-          this.filteredCharacters = data.data.results;
+          this.allCharacters = data.data.results;
+          this.updateTotalPages();
+          this.updateFilteredCharacters();
         } else {
           this.filteredCharacters = [];
         }
       });
   }
 
-
-
   getMarvelData(url?: string): Observable<MarvelData> {
-    if (url) {
-      return this.http.get<MarvelData>(url);
+    const limit = this.charactersPerPage;
+    const offset = (this.currentPage - 1) * this.charactersPerPage;
+
+    let apiUrl = `${this.apiUrl}?ts=1&apikey=${this.apiKey}&hash=6243916182e91659aa5ee22aef120b20&limit=${limit}&offset=${offset}`;
+
+    // Ajoute la logique pour le filtre par lettre
+    const searchLetter = this.searchCtrl.value;
+    if (searchLetter) {
+      apiUrl += `&nameStartsWith=${searchLetter}`;
     }
 
-    const apiUrl = `${this.apiUrl}?ts=1&apikey=${this.apiKey}&hash=6243916182e91659aa5ee22aef120b20`;
     return this.http.get<MarvelData>(apiUrl);
   }
 
   searchMarvelByName(name: string): Observable<MarvelData> {
-    const url = `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${this.apiKey}&hash=6243916182e91659aa5ee22aef120b20&nameStartsWith=${name}`;
+    const offset = (this.currentPage - 1) * this.charactersPerPage;
+    const limit = this.charactersPerPage;
+    const url = `https://gateway.marvel.com/v1/public/characters?ts=1&apikey=${this.apiKey}&hash=6243916182e91659aa5ee22aef120b20&limit=${limit}&offset=${offset}&nameStartsWith=${name}`;
     return this.getMarvelData(url);
   }
 
   searchMarvelCharacters(searchValue: string): Observable<MarvelData> {
-    return this.searchMarvelByName(searchValue);
+    const limit = this.charactersPerPage;
+    const offset = (this.currentPage - 1) * this.charactersPerPage;
+    
+    const url = `${this.apiUrl}?ts=1&apikey=${this.apiKey}&hash=6243916182e91659aa5ee22aef120b20&limit=${limit}&offset=${offset}&nameStartsWith=${searchValue}`;
+    
+    return this.http.get<MarvelData>(url);
+  }
+  
+
+  updateFilteredCharacters() {
+    const startIdx = (this.currentPage - 1) * this.charactersPerPage;
+    const endIdx = startIdx + this.charactersPerPage;
+    this.filteredCharacters = this.allCharacters.slice(startIdx, endIdx);
+    console.log('Filtered Characters:', this.filteredCharacters);
+  }
+  
+
+
+  updateTotalPages() {
+    this.totalPages = Math.ceil((this.marvelData?.data?.total || 0) / this.charactersPerPage) || 1;
   }
 
   onSearchChange() {
-    const searchValue = this.searchCtrl.value;
-    if (searchValue) {
-      this.searchMarvelCharacters(searchValue).subscribe((data: MarvelData) => {
-        this.filteredCharacters = data.data.results;
+    const searchLetter = this.searchCtrl.value;
+    if (searchLetter) {
+      this.searchMarvelCharacters(searchLetter).subscribe((data: MarvelData) => {
+        this.allCharacters = data.data.results;
+        this.updateTotalPages();
+        this.updateFilteredCharacters();
       });
     } else {
-      this.filteredCharacters = this.allCharacters;
+      this.getMarvelData().subscribe((data: MarvelData) => {
+        this.marvelData = data;
+        this.allCharacters = data.data.results;
+        this.updateTotalPages();
+        this.updateFilteredCharacters();
+      });
     }
+  }
+  
+  
+
+  onPageChange(page: number) {
+    this.zone.run(() => {
+      console.log('Clicked on page:', page);
+      this.currentPage = page;
+      this.updateFilteredCharacters();
+    });
+  }
+
+  getPagesArray(): number[] {
+    return Array.from({ length: this.totalPages }, (_, i) => i + 1);
   }
 }
